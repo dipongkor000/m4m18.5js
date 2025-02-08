18e06aeaa321b45a28e3c44beaee112051eaf9eb
dipongkor000 <dipongkorroy000@gmail.com>
2025-02-08 14:41:27 +0600

    file name change and file add

---
+++ first4-1.js
@@ -0,0 +1,10 @@
+let friends = ['kalam', 'salam', 'aklam'];
+console.log(friends);
+
+// first element out the array
+friends.shift();
+console.log(friends);
+
+// add element from last
+friends.unshift('raju');
+console.log(friends);
--- frist4.js
+++
@@ -1,3 +0,0 @@
-let ages = [];
-let numbers = [12,87,98,45];
-console.log(numbers);
