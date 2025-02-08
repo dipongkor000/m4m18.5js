19e1bc8ad47c7c77e24bb6cda01e64b34e633528
dipongkor000 <dipongkorroy000@gmail.com>
2025-02-08 16:19:43 +0600

    add file

---
+++ first5.js
@@ -0,0 +1,24 @@
+const friends = ['raju', 'saju', 'jadu', 'modu']
+//locking for someone 
+console.log(friends.includes('jadu'));
+console.log(friends.includes("rahim"))
+
+//locking for someone, after party
+// if(friends.includes('modu')){
+//     console.log('party');
+// }
+// else{
+//     console.log('no party');
+// }
+
+//locking for someone position or indexof
+console.log(friends.indexOf('saju'));
+// -1 if there is nothing
+console.log(friends.indexOf('rahim'));
+
+let fullName = [];
+let foodName = 25;
+// array test
+console.log(Array.isArray(friends));
+console.log(Array.isArray(fullName));
+console.log(Array.isArray(foodName));
