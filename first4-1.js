8fc086d6ac8e981b09ac371facf5c90f67c982a5
Dipongkor roy <dipongkorroy000@gmail.com>
2025-02-12 01:31:22 +0600

    Update first4-1.js

--- first4-1.js
+++ first4-1.js
@@ -17,3 +17,10 @@ console.log(friends);
 let out1 = friends.pop();
 console.log(friends);
 
+//combining arrays
+let array1 = [1, 2, 3];
+let array2 = [4, 5, 6];
+
+let combinedArray = array1.concat(array2);
+
+console.log(combinedArray)
