05d5521b10dfc2950b864d4c0b5b907c6ed40f82
Dipongkor roy <dipongkorroy000@gmail.com>
2025-02-12 01:24:29 +0600

    Update first4-1.js

--- first4-1.js
+++ first4-1.js
@@ -1,19 +1,19 @@
 let friends = ['kalam', 'salam', 'aklam'];
 console.log(friends);
 
-// first element out the array --->
-friends.shift();
-console.log(friends);
-
-// last element out the array --->
-let out1 = friends.pop();
-console.log(friends);
-
 // add element from first --->
 friends.unshift('raju');
 console.log(friends);
 
 // add element from last --->
 friends.push('rahim');
+console.log(friends);
+
+// first element out the array --->
+friends.shift();
+console.log(friends);
+
+// last element out the array --->
+let out1 = friends.pop();
 console.log(friends);
 
